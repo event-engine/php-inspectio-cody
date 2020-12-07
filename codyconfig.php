18b8f5072561b6db8db3ee68aa84e897e9ddbf70
Sandro Keil <github@sandro-keil.de>
2020-12-07 10:30:25 +0100

    Update example

--- codyconfig.php
+++ codyconfig.php
@@ -1,40 +1,24 @@
 <?php
 
-use EventEngine\CodeGenerator\Cartridge\EventEngine\Filter\Noop;
-use EventEngine\CodeGenerator\Cartridge\EventEngine\Filter\NormalizeLabel;
-use EventEngine\Cody\CodyConfig;
+/**
+ * @see       https://github.com/event-engine/php-inspectio-cody for the canonical source repository
+ * @copyright https://github.com/event-engine/php-inspectio-cody/blob/master/COPYRIGHT.md
+ * @license   https://github.com/event-engine/php-inspectio-cody/blob/master/LICENSE.md MIT License
+ */
 
-use Laminas\Filter;
+declare(strict_types=1);
 
-$context = new stdClass();
-$context->srcFolder = 'app';
-$context->appNamespace ='MyService';
+use EventEngine\InspectioCody\CodyConfig;
 
-// default filters
-$context->filterConstName = new Filter\FilterChain();
-$context->filterConstName->attach(new NormalizeLabel());
-$context->filterConstName->attach(new Filter\Word\SeparatorToSeparator(' ', ''));
-$context->filterConstName->attach(new Filter\Word\CamelCaseToUnderscore());
-$context->filterConstName->attach(new Filter\Word\DashToUnderscore());
-$context->filterConstName->attach(new Filter\StringToUpper());
-
-$context->filterConstValue = new Filter\FilterChain();
-$context->filterConstValue->attach(new NormalizeLabel());
-$context->filterConstValue->attach(new Filter\Word\SeparatorToSeparator(' ', '-'));
-$context->filterConstValue->attach(new Filter\Word\UnderscoreToCamelCase());
-$context->filterConstValue->attach(new Filter\Word\DashToCamelCase());
-
-$context->filterDirectoryToNamespace = new Filter\FilterChain();
-$context->filterDirectoryToNamespace->attach(new Filter\Word\SeparatorToSeparator(DIRECTORY_SEPARATOR, '|'));
-$context->filterDirectoryToNamespace->attach(new Filter\Word\SeparatorToSeparator('|', '\\\\'));
-
-$context->filterNamespaceToDirectory = new Noop();
+$context = new stdClass(); // replace it with your own context class
 
 return new CodyConfig(
     $context,
     [
-        CodyConfig::HOOK_ON_COMMAND => require 'example/hooks/command.php',
-        CodyConfig::HOOK_ON_AGGREGATE => require 'example/hooks/aggregate.php',
-        CodyConfig::HOOK_ON_EVENT => require 'example/hooks/event.php',
+//        CodyConfig::HOOK_ON_AGGREGATE => new AggregateHook(),
+//        CodyConfig::HOOK_ON_COMMAND => new CommandHook(),
+//        CodyConfig::HOOK_ON_EVENT => new EventHook(),
+//        CodyConfig::HOOK_ON_POLICY => new PolicyHook(),
+//        CodyConfig::HOOK_ON_DOCUMENT => new DocumentHook(),
     ]
 );
